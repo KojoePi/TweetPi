2e6a1fcc2ee5521dddb0a759756a38f7c7e51458
KojoePi <kojoebc@googlemail.com>
2015-05-22 13:36:46 +0200

    Update uptime.py
    Added comments in english

--- uptime.py
+++ uptime.py
@@ -5,15 +5,18 @@ import os
 import time
 from twython import Twython
 
+#A script that will tweet the uptime of your orange pi
+
 CONSUMER_KEY = 'YOUR CONSUMER KEY'
 CONSUMER_SECRET = 'YOUR CONSUMER SECRET'
 ACCESS_KEY = 'YOUR KEY'
 ACCESS_SECRET = 'YOUR SECRET'
 
 api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
-
+#Setting up a variable 
 cmd = 'uptime'
+#Formate the variable
 uptime = os.popen(cmd).readline()
 
-# Send a Tweet with the result of uptime
+# Send a Tweet with the result of uptime and some additional text.
 api.update_status(status='default@orangepiplus:/# '+uptime+'#orangepi #bot')
