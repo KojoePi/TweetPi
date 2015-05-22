80b3a4473a7f1bc98c3646926ea1ca6b94c03b9c
KojoePi <kojoebc@googlemail.com>
2015-05-22 13:30:33 +0200

    Update cpu.py
    Added comments

--- cpu.py
+++ cpu.py
@@ -5,14 +5,17 @@ import os
 import time
 from twython import Twython
 
+#This is a script that will tweet the actuall CPU THemperature of the Orange Pi.
 CONSUMER_KEY = 'YOUR CONSUMER KEY'
 CONSUMER_SECRET = 'YOUR CONSUMER SECRET'
 ACCESS_KEY = 'YOUR KEY'
 ACCESS_SECRET = 'YOUR SECRET'
 
 api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
+#Setting up the cmd variable containig a simple bash command.
 cmd = 'cat --squeeze-blank /sys/class/thermal/thermal_zone0/temp'
+#Formate the variable cmd
 temp = os.popen(cmd).readline().strip()
 
-# Tweet mit CPU Temperatur
-api.update_status(status='Meine CPU ist '+temp+'° C warm. #orangepi #bot')
+# The actuall tweet
+api.update_status(status='My CPU is '+temp+'° C hot. #orangepi #bot')
